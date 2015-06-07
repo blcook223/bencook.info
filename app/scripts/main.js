3bc2e3bc281fd20aed7accbc20b09b15d9daae48
blcook223 <benjamin.l.cook@gmail.com>
2015-06-06 19:02:21 -0500

    updates to site, including new portfolio item

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -48,11 +48,11 @@
     locations = [
       {
         position: new google.maps.LatLng(32.75492, -97.33070),
-        content: '<a href="http://www.beerknurd.com/stores/fortworth/">Flying Saucer</a>'
+        content: '<a href="http://www.beerknurd.com/stores/fortworth/" target="_blank">Flying Saucer</a>'
       },
       {
         position: new google.maps.LatLng(32.67513, -97.41086),
-        content: '<a href="http://www.rosascafe.com/home">Rosa\'s Cafe</a>'
+        content: '<a href="http://www.rosascafe.com/home" target="_blank">Rosa\'s Cafe</a>'
       },
       {
         position: new google.maps.LatLng(32.60990, -97.39548),
@@ -60,7 +60,7 @@
       },
       {
         position: new google.maps.LatLng(32.751587, -97.082996),
-        content: '<a href="http://texas.rangers.mlb.com/tex/ballpark/">Ballpark in Arlington</a>'
+        content: '<a href="http://texas.rangers.mlb.com/tex/ballpark/" target="_blank">Ballpark in Arlington</a>'
       }
     ];
 
