81ce3701edb446e56ec3bef0371a1765b5262afb
blcook223 <benjamin.l.cook@gmail.com>
2015-06-23 22:46:48 -0500

    added personal app and models

---
+++ personal/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Event(models.Model):
+    pass
+
+
+class Project(models.Model):
+    pass
+
+
+class Skill(models.Model):
+    """
+    Technical or professional skill
+    """
+    TECHNICAL = 'T'
+    PROFESSIONAL = 'P'
+    CATEGORY_CHOICES = (
+        (TECHNICAL, 'Technical'),
+        (PROFESSIONAL, 'Professional'),
+    )
+    BEGINNER = '1'
+    INTERMEDIATE = '2'
+    COMPETENT = '3'
+    PROFICIENT = '4'
+    EXPERT = '5'
+    LEVEL_CHOICES = (
+        (BEGINNER, 'Beginner (1)'),
+        (INTERMEDIATE, 'Intermediate (2)'),
+        (COMPETENT, 'Competent (3)'),
+        (PROFICIENT, 'Proficient (4)'),
+        (EXPERT, 'Expert (5)'),
+    )
+    category = models.CharField(max_length=1,
+                                choices=CATEGORY_CHOICES,
+                                default=TECHNICAL)
+    level = models.CharField(max_length=1,
+                             choices=LEVEL_CHOICES,
+                             default=BEGINNER)
+    name = models.CharField(max_length=200)
+
+
+class Testimonial(models.Model):
+    pass
+
+
+class Job(models.Model):
+    pass
