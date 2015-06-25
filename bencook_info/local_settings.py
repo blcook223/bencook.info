8109279569c6ebf658135fe913089f731da3da2d
blcook223 <benjamin.l.cook@gmail.com>
2015-06-24 20:38:06 -0500

    added local settings

---
+++ bencook_info/local_settings.py
@@ -0,0 +1,10 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bencook_info_dev',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '192.168.33.10',
+    }
+}
