6f9bf71144a7978eccc846154a57e16f572cde8b
blcook223 <benjamin.l.cook@gmail.com>
2015-06-30 23:11:37 -0500

    personal model and admin interface changes

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,108 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Skill,
+    Image,
+    Event,
+    Technology,
+    Testimonial,
+    Job,
+    Project,
+    Link,
+    OpenSourceContribution,
+)
+
+
+class LinkAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Link model
+    """
+    fields = ['href', 'text', 'title', 'target']
+    list_display = ['title', 'text', 'href']
+
+
+class ImageAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Image model
+    """
+    fields = ['title', 'alt', 'image_file', 'link']
+
+
+class EventAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Event model
+    """
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Dates', {'fields': ['start_date', 'end_date']}),
+        ('Details', {'fields': ['description', 'html', 'images']}),
+    ]
+    list_display = ['name', 'start_date', 'end_date']
+    filter_horizontal = ['images']
+
+
+class JobAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Job model
+    """
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Dates', {'fields': ['start_date', 'end_date']}),
+        ('Details', {'fields': ['employer', 'category']}),
+    ]
+    list_display = ['title', 'employer', 'start_date', 'end_date']
+
+
+class TechologyAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Technology model
+    """
+    fields = ['name', 'category']
+    list_display = ['name', 'category']
+
+
+class OpenSourceContributionAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for OpenSourceContribution model
+    """
+    fields = ['name', 'link']
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Project model
+    """
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Dates', {'fields': ['start_date', 'end_date']}),
+        ('Details', {'fields': ['contribution', 'description', 'technologies', 'links']}),
+    ]
+    list_display = ['name', 'start_date', 'end_date']
+    filter_horizontal = ['technologies', 'links']
+
+
+class SkillAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Skill model
+    """
+    fields = ['name', 'category', 'level']
+    list_display = ['name', 'category', 'level']
+
+
+class TestimonialAdmin(admin.ModelAdmin):
+    """
+    Administrative interface for Testimonial model
+    """
+    fields = ['source', 'quote']
+    list_display = ['source', 'teaser']
+
+
+admin.site.register(Link, LinkAdmin)
+admin.site.register(OpenSourceContribution, OpenSourceContributionAdmin)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Technology)
+admin.site.register(Testimonial, TestimonialAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Job, JobAdmin)
--- personal/models.py
+++ personal/models.py
@@ -1,12 +1,140 @@
 from django.db import models
+from django.utils import timezone
 
 
-# class Event(models.Model):
-#     pass
+class Link(models.Model):
+    """
+    A link
+    """
+    title = models.CharField(max_length=200,
+                             null=True,
+                             blank=True)
+    href = models.CharField('URL',
+                            max_length=200,
+                            null=True,
+                            blank=True)
+    text = models.CharField(max_length=200,
+                            null=True,
+                            blank=True)
+    target = models.BooleanField('Open in new tab',
+                                 default=False)
+
+    def __str__(self):
+        return '{} ({})'.format(self.title, self.href)
+
+    def save(self, *args, **kwargs):
+        if not self.title:
+            self.title = self.text()
+        super(Link, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('text',)
 
 
-# class Project(models.Model):
-#     pass
+class Image(models.Model):
+    """
+    An image
+    """
+    title = models.CharField(max_length=200,
+                             null=True,
+                             blank=True)
+    image_file = models.ImageField()
+    alt = models.CharField('alt text',
+                           max_length=500)
+    link = models.ForeignKey(Link,
+                             null=True,
+                             blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    """
+    A biographical event
+    """
+    name = models.CharField(max_length=200)
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now,
+                                null=True,
+                                blank=True)
+    description = models.TextField()
+    html = models.TextField('HTML content')
+    images = models.ManyToManyField(Image)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-start_date', '-end_date')
+
+
+class Technology(models.Model):
+    """
+    The name of a technology used in a project
+    """
+    LANGUAGE = 'A'
+    FRAMEWORK = 'B'
+    DATABASE = 'C'
+    EDITOR = 'D'
+    OTHER = 'E'
+    CATEGORY_CHOICES = (
+        (LANGUAGE, 'Language'),
+        (FRAMEWORK, 'Framework'),
+        (DATABASE, 'Database'),
+        (EDITOR, 'Editor'),
+        (OTHER, 'Other'),
+    )
+
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=1,
+                                choices=CATEGORY_CHOICES,
+                                default=LANGUAGE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('category', 'name')
+        verbose_name_plural = 'technologies'
+        unique_together = (('name', 'category'),)
+
+
+class OpenSourceContribution(models.Model):
+    """
+    An open-source project to which a contribution has been
+    made
+    """
+    name = models.CharField(max_length=200)
+    link = models.ForeignKey(Link)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'open-source contribution'
+
+
+class Project(models.Model):
+    """
+    A project in the portfolio
+    """
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now,
+                                null=True,
+                                blank=True)
+    name = models.CharField(max_length=200)
+    contribution = models.CharField(max_length=200)
+    technologies = models.ManyToManyField(Technology)
+    description = models.TextField()
+    links = models.ManyToManyField(Link)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-end_date', 'start_date')
 
 
 class Skill(models.Model):
@@ -15,9 +143,11 @@ class Skill(models.Model):
     """
     TECHNICAL = 'T'
     PROFESSIONAL = 'P'
+    LANGUAGE = 'L'
     CATEGORY_CHOICES = (
         (TECHNICAL, 'Technical'),
         (PROFESSIONAL, 'Professional'),
+        (LANGUAGE, 'Language')
     )
     BEGINNER = '1'
     INTERMEDIATE = '2'
@@ -25,27 +155,54 @@ class Skill(models.Model):
     PROFICIENT = '4'
     EXPERT = '5'
     LEVEL_CHOICES = (
-        (BEGINNER, 'Beginner (1)'),
-        (INTERMEDIATE, 'Intermediate (2)'),
-        (COMPETENT, 'Competent (3)'),
-        (PROFICIENT, 'Proficient (4)'),
-        (EXPERT, 'Expert (5)'),
+        (BEGINNER, 'Beginner ({})'.format(BEGINNER)),
+        (INTERMEDIATE, 'Intermediate ({})'.format(INTERMEDIATE)),
+        (COMPETENT, 'Competent ({})'.format(COMPETENT)),
+        (PROFICIENT, 'Proficient ({})'.format(PROFICIENT)),
+        (EXPERT, 'Expert ({})'.format(EXPERT)),
     )
     category = models.CharField(max_length=1,
                                 choices=CATEGORY_CHOICES,
-                                default=TECHNICAL)
+                                default=LANGUAGE)
     level = models.CharField(max_length=1,
                              choices=LEVEL_CHOICES,
                              default=BEGINNER)
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
-# class Testimonial(models.Model):
-#     """
-#     Testimony from a customer or coworker
-#     """
-#     pass
+    class Meta:
+        ordering = ('category', 'level', 'name')
 
 
-# class Job(models.Model):
-#     pass
+class Testimonial(models.Model):
+    """
+    Testimony from a customer or coworker
+    """
+    quote = models.TextField()
+    source = models.CharField(max_length=200)
+
+    def teaser(self):
+        return self.quote[:50] + '...'
+
+    def __str__(self):
+        return self.source
+
+
+class Job(models.Model):
+    """
+    A professional position
+    """
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now,
+                                null=True,
+                                blank=True)
+    title = models.CharField(max_length=200)
+    employer = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{}, {}'.format(self.title, self.employer)
+
+    class Meta:
+        ordering = ('-end_date', 'start_date')
