5a88399524c2533b4e4104884b52f8bb71072e5a
blcook223 <benjamin.l.cook@gmail.com>
2015-08-20 12:01:03 -0500

    some config changes

--- config.rb
+++ config.rb
@@ -7,7 +7,12 @@ sass_options = (environment == :production) ? {} : {:debug_info => true}
 Sass::Script::Number.precision = 10
 
 
-# add_import_path 'sass'
+add_import_path 'core/static/core/styles'
+add_import_path 'personal/static/personal/styles'
+add_import_path 'blog/static/blog/styles'
 add_import_path 'bower_components'
 
 asset_cache_buster = :none
+
+http_path = '/'
+sass_dir = 'sass'
